4bb565b5123015fe2414df86e75f95aedb0f3640
Jharby Saravia <100247934+jharbyjoel@users.noreply.github.com>
2024-03-31 18:13:20 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import sys
+import argparse
+import socket
+import signal
+
+import confundo
+
+# for testing, replace socket.socket with confundo.Socket()
+# or just use the reference server (check Piazza or consult the instructor)
+
+# the server can only work in a single threaded mode, one client at a time (no parallel,
+# neither concurrent nor threaded---simplifications in the socket implementation)
+
+# Other than that, standard socket interface should work
+
+def start():
+    pass
+
+if __name__ == '__main__':
+    start()
