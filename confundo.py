17afbafb3ee73920abee1efbc34c7db29ff8621f
Jharby Saravia <100247934+jharbyjoel@users.noreply.github.com>
2024-03-31 18:38:32 -0400

    Update confundo.py

--- confundo.py
+++ confundo.py
@@ -1,82 +1,57 @@
 import socket
 import struct
 import time
-import random
 
 # Constants
-SYN = 0x01
-ACK = 0x02
-FIN = 0x04
-HEADER_FORMAT = "!IIB"
-HEADER_SIZE = 9
-MTU = 424
-PAYLOAD_SIZE = MTU - HEADER_SIZE
-SEQ_NUM_MAX = 50000
-INITIAL_CWND = PAYLOAD_SIZE
-SS_THRESH = 12000  # Slow start threshold
+MAX_UDP_SIZE = 424
+MTU_SIZE = 412
+MAX_SEQ_NUM = 50000
+INIT_CWND = 412
+INIT_SS_THRESH = 12000
+INIT_SEQ_NUM = 50000
+
+# Flags
+ACK = 0b100
+SYN = 0b010
+FIN = 0b001
 
 class Socket:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.remote_addr = None
-        self.seq_num = random.randint(0, SEQ_NUM_MAX)
+        self.seq_num = INIT_SEQ_NUM
         self.ack_num = 0
-        self.cwnd = INITIAL_CWND
-        self.ssthresh = SS_THRESH
-        self.send_base = 0
-        self.timeout = 1.0  # Initial retransmission timeout value
-        self.unacknowledged_packets = {}
+        self.conn_id = 0
+        self.cwnd = INIT_CWND
+        self.ss_thresh = INIT_SS_THRESH
 
     def settimeout(self, timeout):
         self.sock.settimeout(timeout)
 
-    def connect(self, address):
-        # Connection establishment logic remains unchanged
+    def bind(self, address):
+        self.sock.bind(address)
+
+    def listen(self, backlog):
         pass
 
-    def _packetize(self, data):
-        # Split data into segments according to CWND and create packets
-        segments = [data[i:i+PAYLOAD_SIZE] for i in range(0, len(data), PAYLOAD_SIZE)]
-        packets = []
-        for segment in segments:
-            packet = struct.pack(HEADER_FORMAT, self.seq_num, 0, ACK) + segment
-            packets.append(packet)
-            self.seq_num = (self.seq_num + len(segment)) % SEQ_NUM_MAX
-        return packets
+    def accept(self):
+        pass
 
-    def send(self, data):
-        packets = self._packetize(data)
-        for packet in packets:
-            self._send_and_wait_ack(packet)
+    def connect(self, address):
+        self.send(b'', flags=SYN)
 
-    def _send_and_wait_ack(self, packet):
-        while True:
-            self.sock.sendto(packet, self.remote_addr)
-            self.unacknowledged_packets[self.seq_num] = (packet, time.time())
-            try:
-                while True:
-                    received_packet, _ = self.sock.recvfrom(MTU)
-                    ack_num = struct.unpack(HEADER_FORMAT, received_packet[:HEADER_SIZE])[1]
-                    if ack_num in self.unacknowledged_packets:
-                        del self.unacknowledged_packets[ack_num]
-                        # Adjust CWND according to congestion control algorithm
-                        if self.cwnd < self.ssthresh:
-                            self.cwnd += PAYLOAD_SIZE  # Slow start phase
-                        else:
-                            self.cwnd += int(PAYLOAD_SIZE**2 / self.cwnd)  # Congestion avoidance
-                        return  # Move to the next packet
-            except socket.timeout:
-                # Retransmission due to timeout
-                self.ssthresh = max(self.cwnd // 2, 1 * PAYLOAD_SIZE)  # Update ssthresh
-                self.cwnd = INITIAL_CWND  # Reset CWND to initial size
-                # Resend all unacknowledged packets
-                for seq, (pkt, _) in self.unacknowledged_packets.items():
-                    self.sock.sendto(pkt, self.remote_addr)
+    def send(self, data, flags=0):
+        packet = self._build_packet(data, flags)
+        self.sock.send(packet)
+
+    def recv(self, bufsize):
+        return self.sock.recv(bufsize)
 
     def close(self):
-        # Connection termination logic remains unchanged
-        pass
+        self.sock.close()
 
+    def _build_packet(self, data, flags):
+        header = struct.pack('!IIHBB', self.seq_num, self.ack_num, self.conn_id, 0, flags)
+        return header + data
 
 
 
